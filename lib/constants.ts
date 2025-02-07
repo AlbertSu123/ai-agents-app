07d71b4cabcefdfba9820005282d191e44ce4476
Albert Su <albertsu2001@gmail.com>
2025-02-07 14:47:20 -0800

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,9 +2,9 @@ import { base } from 'viem/chains'
 import { createPublicClient, Hex, http } from 'viem'
 import { privateKeyToAccount } from 'viem/accounts'
 
-export const API_URL = 'http://localhost:80/api'
-// export const API_URL =
-// 	'https://cd95diw9kg.us-east-1.awsapprunner.com/api'
+// export const API_URL = 'http://localhost:80/api'
+export const API_URL =
+	'https://cd95diw9kg.us-east-1.awsapprunner.com/api'
 export const DEFAULT_CHAIN = base
 
 export const account = privateKeyToAccount(
